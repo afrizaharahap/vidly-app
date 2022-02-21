6e70478666959c231cd1c57d6d8e8b574352e13b
afrizaharahap <afriza.harahap@gmail.com>
2022-02-21 17:25:59 +0700

    update index.js

--- src/App.js
+++ src/App.js
@@ -16,14 +16,14 @@ class App extends Component {
         <NavBar />
         <main className="container">
           <Switch>
-            <Route path="/login" component={LoginForm} />
-            <Route path="/movies/:id" component={MovieForm} />
-            <Route path="/movies" component={Movies} />
-            <Route path="/customers" component={Customers} />
-            <Route path="/rentals" component={Rentals} />
-            <Route path="/not-found" component={NotFound} />
-            <Redirect from="/" exact to="/movies" />
-            <Redirect to="/not-found" />
+            <Route path={"/login"} component={LoginForm} />
+            <Route path={"/movies/:id"} component={MovieForm} />
+            <Route path={"/movies"} component={Movies} />
+            <Route path={"/customers"} component={Customers} />
+            <Route path={"/rentals"} component={Rentals} />
+            <Route path={"/not-found"} component={NotFound} />
+            <Redirect from={"/"} exact to="/movies" />
+            <Redirect to={"/not-found"} />
           </Switch>
         </main>
       </React.Fragment>

7faaf75ff319de645a3037f8bb576629e32aeee8
afrizaharahap <afriza.harahap@gmail.com>
2022-02-22 21:03:10 +0700

    extracting table

--- src/components/common/input.jsx
+++ src/components/common/input.jsx
@@ -14,6 +14,17 @@ const Input = ({ name, label, value, error, onChange }) => {
       {error && <div className="alert alert-danger">{error}</div>}
     </div>
   );
+  // handleChange = ({ currentTarget: input }) => {
+  //   const errors = { ...this.state.errors };
+  //   const errorMessage = this.validateProperty(input);
+
+  //   if (errorMessage) errors[input.name] = errorMessage;
+  //   else delete errors[input.name];
+
+  //   const data = { ...this.state.data };
+  //   data[input.name] = input.value;
+  //   this.setState({ data, errors });
+  // };
 };
 
 export default Input;
